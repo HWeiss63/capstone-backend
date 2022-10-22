b104d593f679bca8fb0aa0f09b32d880c5fd8873
Hwieun <dms7122@gmail.com>
2022-10-22 11:35:24 -0400

    backend deployed

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -11,32 +11,4 @@ class ChecklistsController < ApplicationController
     end
     render json: data.as_json
   end
-
-  # def create
-  #   p "this is a test"
-  #   params.except(:controller, :action, :checklist).each do |key, value|
-  #     p "-----"
-  #     p key
-  #     value.each do |item|
-  #       p "#######"
-  #       p item["checklist"]["id"]
-  #       p current_user.id
-  #       p item["answer"]
-  #     end
-  #     #p key
-  #   end #[1]["checklist"]["id"]
-  # end
-
-  # def index
-  #   checklists = Checklist.all
-  #   answers = []
-  #   checklists.each do |checklist|
-  #     answer = Response.find_by(checklist_id: checklist.id, user_id: current_user.id)
-  #     if answer
-  #       answers << answer
-  #     end
-  #   end
-  #   render json: { checklists: checklists, answers: answers }
-  #   # render json: checklists.as_json
-  # end
 end
